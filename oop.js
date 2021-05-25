28dabd4c66b2865980f042f7ee902af7630d77f9
Kristien Kellens <kristien.kellens@gmail.com>
2021-05-25 17:15:06 +0200

    refactored current code to OOP object with event listeners (still need to look up date object-

--- oop.js
+++ oop.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const startBtn = document.querySelector(".btn-start");
 const stopBtn = document.querySelector(".btn-stop");
 const resetBtn = document.querySelector(".btn-reset");
@@ -10,25 +12,77 @@ const displayMinutes = document.querySelector(".minutes");
 function Stopwatch() {
     //properties
     let running = false;
+    let seconds = 0;
+    let minutes = 0;
     let lapCounter = 0;
-    let timer;
+    let previousMinutes = 0;
+    let previousSeconds = 0;
+    let timer; //variable for set/clear Interval
 
     //methods
+    this.runTimer = function () {
+        //show seconds
+        seconds++;
+        displaySeconds.innerText = seconds.toLocaleString("en-US", { minimumIntegerDigits: 2 });
+
+        //show minutes
+        if (seconds === 60) {
+            displaySeconds.innerText = (0).toLocaleString("en-US", { minimumIntegerDigits: 2 });
+            seconds = 0;
+
+            minutes++;
+            displayMinutes.innerText = minutes.toLocaleString("en-US", { minimumIntegerDigits: 2 });
+        }
+
+        //in case timer goes over 60min, restart count at 00:00
+        if (minutes === 60) {
+            displaySeconds.innerText = (0).toLocaleString("en-US", { minimumIntegerDigits: 2 });
+            seconds = 0;
+
+            displayMinutes.innerText = (0).toLocaleString("en-US", { minimumIntegerDigits: 2 });
+            minutes = 0;
+        }
+
+    }
+
+
     this.start = function () {
-        //startcode here
         if (!running) {
             running = true;
-            console.log("running")
-            //timer = setInterval(runTimer, 1000);
+            timer = setInterval(this.runTimer, 1000);
         }
     };
 
     this.stop = function () {
         //stop code here
+        running = false;
+        console.log("stop timer");
+        clearInterval(timer);
     };
 
     this.lap = function () {
-        //lap code here
+        if (running) {
+            lapCounter++;
+
+            //calculate currenttime - previoustime
+            let diffMinutes = displayMinutes.innerText - previousMinutes;
+            let diffSeconds = displaySeconds.innerText - previousSeconds;
+
+
+            //create new li in ul.laps
+            let lapsUl = document.querySelector(".laps");
+            let li = document.createElement('li');
+
+            li.innerText = `Lap ${lapCounter}: ${diffMinutes.toLocaleString("en-US", { minimumIntegerDigits: 2 })} : ${diffSeconds.toLocaleString("en-US", { minimumIntegerDigits: 2 })}`;
+            lapsUl.appendChild(li);
+
+            //make ul.laps visible
+            lapsUl.style.visibility = "visible";
+
+            //change currenttime to previoustime
+            previousMinutes = displayMinutes.innerText;
+            previousSeconds = displaySeconds.innerText;
+        }
     }
 
     this.reset = function () {
@@ -40,4 +94,10 @@ function Stopwatch() {
 
 const stopwatch = new Stopwatch();
 
-startBtn.addEventListener(onclick,)
+startBtn.addEventListener('click', () => { stopwatch.start(); });
+
+stopBtn.addEventListener('click', () => { stopwatch.stop() });
+
+resetBtn.addEventListener('click', () => { stopwatch.reset() });
+
+lapBtn.addEventListener('click', () => { stopwatch.lap() });
