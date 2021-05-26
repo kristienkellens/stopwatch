090718ac3007edf2647dd36d54fceed502d2de05
Kristien Kellens <kristien.kellens@gmail.com>
2021-05-26 13:06:33 +0200

    fixed stop() resume issue

--- oop.js
+++ oop.js
@@ -7,23 +7,23 @@ const lapBtn = document.querySelector(".btn-lap");
 const displaySeconds = document.querySelector(".seconds");
 const displayMinutes = document.querySelector(".minutes");
 
+
 /** object Stopwatch */
 
 function Stopwatch() {
-    //properties
     let running = false;
     let lapCounter = 0;
     let timer; //variable for set/clear Interval
     let startTime, currentTime, lapTime //variables for date objects
+    let secondsStop = 0;
+    let minutesStop = 0;
 
-    //methods
     this.runTimer = function () {
         currentTime = new Date();
-        //time diff in seconds
         let diffTime = Math.round((currentTime - startTime) / 1000);
 
-        let seconds = diffTime % 60;
-        let minutes = Math.floor(diffTime / 60);
+        let seconds = secondsStop + (diffTime % 60);
+        let minutes = minutesStop + (Math.floor(diffTime / 60));
 
         displaySeconds.innerText = seconds.toLocaleString("en-US", { minimumIntegerDigits: 2 });
         displayMinutes.innerText = minutes.toLocaleString("en-US", { minimumIntegerDigits: 2 });
@@ -32,7 +32,7 @@ function Stopwatch() {
     this.start = function () {
         if (!running) {
             running = true;
-            startTime = new Date();
+            startTime = new Date(); //if stop() has run, startTime will already exist
             lapTime = startTime; //for first lap
             timer = setInterval(this.runTimer, 1000);
         }
@@ -41,8 +41,10 @@ function Stopwatch() {
     this.stop = function () {
         //stop code here
         running = false;
-        console.log("stop timer", startTime);
         clearInterval(timer);
+        secondsStop = parseInt(displaySeconds.innerText);
+        minutesStop = parseInt(displayMinutes.innerText);
+        //console.log(typeof minutesStop, secondsStop)
     };
 
     this.lap = function () {
